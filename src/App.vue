83f0029712108902b7e638f6f1340c10715e93a6
Rizky <rizkyaldi0101@gmail.com>
2019-03-20 10:26:27 +0700

    commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+<div id="app">
+    <todo/>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'materialize-css/sass/materialize.scss';
+// eslint-disable-next-line
+import 'materialize-css/dist/js/materialize.min.js';
+
+Vue.use(VueMaterial)
+import todo from './components/todo.vue'
+export default {
+  name: 'app',
+  components: {
+    todo
+  }
+}
+</script>
+
+<style>
+#app {
+  margin: 0 auto;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: cornflowerblue;
+}
+</style>
